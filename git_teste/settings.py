f8e22a04192e88c5de5d8b818304b39c66e57faf
Real Coder <kadafidusabe@gmail.com>
2022-05-31 15:09:24 +0200

    settings modified

--- git_teste/settings.py
+++ git_teste/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #django_herokusettings(locals())
